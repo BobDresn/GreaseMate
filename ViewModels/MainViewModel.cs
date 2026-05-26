812398f40a7c3086d0e01dfe00faebe3dd7816bb
Bob Dresner <bob.dresner1@gmail.com>
2026-05-26 19:39:10 -0400

    Changed Laptops
    Was programming on Mac, didn't realize it would be much harder to do on Rider. Switched to windows laptop.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace GreaseMate.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string vinInput = "";
+
+    [ObservableProperty]
+    private string make = "";
+
+    [ObservableProperty]
+    private string model = "";
+
+    [ObservableProperty]
+    private int year;
+
+    [RelayCommand]
+    private void DecodeVin()
+    {
+        // Temporary fake data
+        Make = "Ford";
+        Model = "Mustang";
+        Year = 2020;
+    }
+}
---
+++ Views/MainView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using GreaseMate.ViewModels;
+using System.Windows.Controls;
+
+namespace GreaseMate.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+
+        DataContext = new MainViewModel();
+    }
+}
